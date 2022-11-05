4be9559ec00d08a07864c8a48b7f160dd07c80d6
Sahil Siddiq <icegambit91@gmail.com>
2022-11-05 19:12:47 +0530

    Add dot product

---
+++ libs/vec_opers.hpp
@@ -0,0 +1,8 @@
+#include "vectors.hpp"
+
+double dot(Vector::Vec3& a, Vector::Vec3& b){
+    double res = a[0] * b[0] +
+                 a[1] * b[1] +
+                 a[2] * b[2];
+    return res;
+}
