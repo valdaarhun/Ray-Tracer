d898d61d8dcae40ec893ba408ace99749c26fd90
Sahil Siddiq <icegambit91@gmail.com>
2022-11-05 19:55:09 +0530

    Make function inline

--- libs/vec_opers.hpp
+++ libs/vec_opers.hpp
@@ -1,6 +1,6 @@
 #include "vectors.hpp"
 
-double dot(Vector::Vec3& a, Vector::Vec3& b){
+inline double dot(Vector::Vec3& a, Vector::Vec3& b){
     double res = a[0] * b[0] +
                  a[1] * b[1] +
                  a[2] * b[2];
