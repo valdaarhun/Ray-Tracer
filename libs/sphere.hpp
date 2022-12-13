0fcb790d4e590e86934676ac4e31e4dfd39c11e2
Sahil Siddiq <icegambit91@gmail.com>
2022-12-13 16:06:53 +0530

    Add class for spherical objects

--- libs/sphere.hpp
+++ libs/sphere.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+
 #include "hittable.hpp"
 #include "vectors.hpp"
 
@@ -9,9 +11,10 @@ class Sphere: public Hittable{
 private:
     Point3 center;
     double radius;
+    std::shared_ptr< Material > material;
 
 public:
-    Sphere(const Point3&, double);
+    Sphere(const Point3&, double, std::shared_ptr< Material >);
 
-    bool hit(const Ray& r, double t_min, bool t_max, Intersection& point) const override;
+    bool hit(const Ray& r, double t_min, double t_max, Intersection& point) const override;
 };
--- src/sphere.cpp
+++ src/sphere.cpp
@@ -1,18 +1,20 @@
 #include <cmath>
+#include <memory>
 
 #include "sphere.hpp"
 #include "vectors.hpp"
 #include "vec_opers.hpp"
 
-Sphere::Sphere(const Point3& center, double radius){
+Sphere::Sphere(const Point3& center, double radius, std::shared_ptr< Material > material){
     this -> center = center;
     this -> radius = radius;
+    this -> material = material;
 }
 
 /*
 * (A + tB - C).(A + tB - C) = radius ^ 2
 */
-bool Sphere::hit(const Ray& r, double t_min, bool t_max, Intersection& point) const{
+bool Sphere::hit(const Ray& r, double t_min, double t_max, Intersection& point) const{
     Point3 ray_origin = r.get_origin();
     Vec3 ray_direction = r.get_direction();
     Vec3 oc = ray_origin - center;
@@ -36,6 +38,7 @@ bool Sphere::hit(const Ray& r, double t_min, bool t_max, Intersection& point) co
     point.point = r.point_on_ray(t);
     Vec3 normal = (point.point - center) / radius;
     point.set_front_side(r, normal);
+    point.material = material;
 
     return true;
 }
